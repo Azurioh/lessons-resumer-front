ffe826e21a0083b37f4b33f67464b1e7457ceaf7
Azurioh <alan.cunin@epitech.eu>
2024-12-20 01:07:10 +0100

    feat: Create enum for the pages path

---
+++ src/enums/pages.ts
@@ -0,0 +1,10 @@
+export enum Pages {
+  home = '/',
+  login = '/login',
+  register = '/register',
+  forgotPassword = '/forgot',
+  resetPassword = '/reset',
+  dashboard = '/dashboard',
+  summarize = '/summarize',
+  summarizeDetail = '/summarize/:id',
+}
