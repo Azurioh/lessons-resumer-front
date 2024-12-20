ab68881abbbde11869188cc19b256594f4af5c3d
Azurioh <alan.cunin@epitech.eu>
2024-12-20 23:34:03 +0100

    feat: Create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import { useTranslation } from "react-i18next";
+import style from "./Footer.module.css";
+
+const Footer = () => {
+    const { t } = useTranslation();
+
+  return (
+    <footer className={style.footer}>
+      <div className={style.footerContainer}>
+        <p className={style.copyright}>© {new Date().getFullYear()} - {t('copyright')}</p>
+        <div className={style.footerRow}>
+          <a href="/faq">{t('footer.faq')}</a>
+          <a href="/contact">{t('footer.contact')}</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
