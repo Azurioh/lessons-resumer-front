9cb35109ea0e257b47195ec343214209ddfc2307
Azurioh <alan.cunin@epitech.eu>
2024-12-21 14:32:06 +0100

    feat: Add background on homepage and update redirect link

--- src/pages/LandingPages/Home/Home.tsx
+++ src/pages/LandingPages/Home/Home.tsx
@@ -4,6 +4,7 @@ import Navbar from '@src/components/Navbar/Navbar';
 import type React from 'react';
 import { useTranslation } from 'react-i18next';
 import style from './Home.module.css';
+import background from '@assets/background/home-background.jpg';
 
 const HomePage: React.FC = () => {
   const { t } = useTranslation();
@@ -16,13 +17,14 @@ const HomePage: React.FC = () => {
           <h1 className={style.title}>{t('home.title')}</h1>
           <p className={style.subtitle}>{t('home.subtitle')}</p>
           <div className={style.buttonContainer}>
-            <a href="/register" className={style.button}>
+            <a href="/login" className={style.button}>
               {t('home.try-it')}
             </a>
             <a href="/about" className={style.button}>
               {t('home.learn-more')}
             </a>
           </div>
+          <img src={background} alt="background" className={style.background} />
         </div>
         <div className={style.aboutContainer}>
           <div className={style.aboutImg}>
