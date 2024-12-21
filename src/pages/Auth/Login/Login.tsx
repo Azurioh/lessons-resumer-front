6f9d2115a01c602cce1939ea07da097d715fcdfc
Azurioh <alan.cunin@epitech.eu>
2024-12-21 14:33:18 +0100

    feat: Update title of login page & add register button for phone view

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -1,6 +1,6 @@
 import background from '@assets/background/login-background.jpg';
-import { Input } from 'antd';
-import React from 'react';
+import { Form, Input } from 'antd';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import style from './Login.module.css';
 
@@ -9,17 +9,22 @@ const LoginPage: React.FC = () => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
 
+  useEffect(() => {
+      document.title = t("login.title");
+    }, [t]);
+
   return (
     <div className={style.container}>
       <div className={style.card}>
         <div className={style.loginForm}>
           <h2>{t('login.title')}</h2>
-          <div className={style.inputs}>
+          <Form className={style.inputs}>
             <Input
               className={style.input}
               type="email"
               placeholder={t('login.email')}
               value={email}
+              required
               onChange={(e) => setEmail(e.target.value)}
             />
             <Input
@@ -27,10 +32,12 @@ const LoginPage: React.FC = () => {
               type="password"
               placeholder={t('login.password')}
               value={password}
+              required
               onChange={(e) => setPassword(e.target.value)}
             />
-            <button type="submit">{t('login.submit')}</button>
-          </div>
+            <button type="submit" className={style.loginButton}>{t('login.submit')}</button>
+            <a href="/register">{t('login.registerHidden')}</a>
+          </Form>
         </div>
         <div className={style.register}>
           <h2>{t('login.notRegistered')}</h2>
