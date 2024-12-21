55935fa40a61a977692543b904bc3471d627a44c
Azurioh <alan.cunin@epitech.eu>
2024-12-21 14:31:45 +0100

    feat: Add new pages in enum

--- src/enums/pages.ts
+++ src/enums/pages.ts
@@ -1,9 +1,16 @@
 export enum Pages {
+  // Landing Pages
   home = '/',
+  about = '/about',
+  services = '/services',
+  faq = '/faq',
+  contact = '/contact',
+  // Auth Pages
   login = '/login',
   register = '/register',
   forgotPassword = '/forgot',
   resetPassword = '/reset',
+  // Summarize Pages
   dashboard = '/dashboard',
   summarize = '/summarize',
   summarizeDetail = '/summarize/:id',
