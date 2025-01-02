9eb73301b47059bb3f65031fe19f1bc4d72e3b56
Azurioh <alan.cunin@epitech.eu>
2025-01-02 01:19:19 +0100

    fix(navbar): Fix navbar redirect links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import Logo from '@icons/logo.png';
+import { Pages } from '@src/enums/pages';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
 import style from './Navbar.module.css';
@@ -13,16 +14,16 @@ const Navbar: React.FC = () => {
           <img src={Logo} alt="logo" className={style.logo} />
         </div>
         <div className={style.navbarLinks}>
-          <a href="/home" className={style.navbarLink}>
+          <a href={Pages.home} className={style.navbarLink}>
             {t('navbar.home')}
           </a>
-          <a href="/about" className={style.navbarLink}>
+          <a href={Pages.about} className={style.navbarLink}>
             {t('navbar.about')}
           </a>
-          <a href="/services" className={style.navbarLink}>
+          <a href={Pages.services} className={style.navbarLink}>
             {t('navbar.services')}
           </a>
-          <a href="/pricing" className={style.navbarLink}>
+          <a href={Pages.pricing} className={style.navbarLink}>
             {t('navbar.pricing')}
           </a>
         </div>
